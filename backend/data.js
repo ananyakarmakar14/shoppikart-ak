eeeef5dbe8dea0af9a122570988e7e1518e137b6
Pobitra Karmakar <pobitra.19je0609@ce.iitism.ac.in>
2022-08-07 17:49:54 +0530

    first commit

---
+++ backend/data.js
@@ -0,0 +1,100 @@
+import bcrypt from "bcryptjs";
+import fs from "fs";
+import path from "path";
+
+const data = {
+  users: [
+    {
+      name: "Pobitra",
+      email: "admin@example.com",
+      password: bcrypt.hashSync("123456"),
+      isAdmin: true,
+    },
+    {
+      name: "John Doe",
+      email: "user@example.com",
+      password: bcrypt.hashSync("123456"),
+      isAdmin: false,
+    },
+  ],
+  products: [
+    {
+      // _id: '1',
+      name: "Nike Slim shirt",
+      slug: "nike-slim-shirt",
+      category: "Shirts",
+      image: "/images/p1.jpg", // 679px × 829px
+      price: 120,
+      countInStock: 10,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality shirt",
+    },
+    {
+      // _id: '2',
+      name: "Adidas Fit Shirt",
+      slug: "adidas-fit-shirt",
+      category: "Shirts",
+      image: "/images/p2.jpg",
+      price: 250,
+      countInStock: 0,
+      brand: "Adidas",
+      rating: 4.0,
+      numReviews: 10,
+      description: "high quality product",
+    },
+    {
+      // _id: '3',
+      name: "Nike Slim Pant",
+      slug: "nike-slim-pant",
+      category: "Pants",
+      image: "/images/p3.jpg",
+      price: 25,
+      countInStock: 15,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 14,
+      description: "high quality product",
+    },
+    {
+      // _id: '4',
+      name: "Adidas Fit Pant",
+      slug: "adidas-fit-pant",
+      category: "Pants",
+      image: "/images/p4.jpg",
+      price: 65,
+      countInStock: 5,
+      brand: "Puma",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality product",
+    },
+  ],
+};
+
+const __dirname = path.resolve();
+let dataset = fs.readFileSync(path.resolve(__dirname, "dataset.json"), {
+  encoding: "utf8",
+  flag: "r",
+});
+
+dataset = JSON.parse(dataset);
+
+dataset.forEach((item, idx) => {
+  let label = item.annotation[0].label[0];
+  data.products.push({
+    name: "Nice " + label ?? "Item" + " " + idx,
+    slug: "item-" + idx + "-" + Date.now(),
+    category: label,
+    image: "/images/p" + (Math.floor(Math.random() * 3) + 1) + ".jpg",
+    price: Math.floor(Math.random() * 100) + 10,
+    countInStock: 5,
+    brand: "Puma",
+    rating: Math.floor(Math.random() * 4) + 0.5,
+    numReviews: Math.floor(Math.random() * 100),
+    description: "high quality product",
+  });
+});
+
+export default data;
---
+++ backend/server.js
@@ -0,0 +1,58 @@
+import express from "express";
+import path from "path";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import seedRouter from "./routes/seedRoutes.js";
+import productRouter from "./routes/productRoutes.js";
+import userRouter from "./routes/userRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
+import uploadRouter from "./routes/uploadRoutes.js";
+import cors from "cors";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+let __dirname = path.resolve();
+
+app.use("/static", express.static("images"));
+app.use(express.static("build"));
+
+app.get("/api/keys/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
+});
+app.get("/api/keys/google", (req, res) => {
+  res.send({ key: process.env.GOOGLE_API_KEY || "" });
+});
+
+app.use("/api/upload", uploadRouter);
+app.use("/api/seed", seedRouter);
+app.use("/api/products", productRouter);
+app.use("/api/users", userRouter);
+app.use("/api/orders", orderRouter);
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/build/index.html"))
+);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`);
+});
